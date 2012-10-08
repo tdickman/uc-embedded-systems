40180faed51f49cf717a04a666794bf157d874ad
zerhusbt <benjamintzerhusen@hotmail.com>
2012-10-08 10:04:31 -0700

    Fixed syntax errors

--- Project2/top_level.v
+++ Project2/top_level.v
@@ -7,7 +7,7 @@
 //
 //
 //
-module FIR_top_level(toggleBtn,CLOCK_50,HEX0,HEX1,HEX2,LEDG);
+module top_level(toggleBtn,CLOCK_50,HEX0,HEX1,HEX2,LEDG);
 
 	//I/O Definition
 	input toggleBtn;
@@ -64,33 +64,33 @@ module FIR_top_level(toggleBtn,CLOCK_50,HEX0,HEX1,HEX2,LEDG);
 	begin
 		case(show7seg)
 			A:begin
-				cur7seg[11:8] <= 4'b1111;
-				cur7seg[7:4] <= 4'b1111;
-				cur7seg[3:0] <= 4'b1111;
+				cur7Seg[11:8] <= 4'b1111;
+				cur7Seg[7:4] <= 4'b1111;
+				cur7Seg[3:0] <= 4'b1111;
 				show7seg = 2'b01;
 			  end
 			B:begin
-				cur7seg[11:8] <= BCD8;
-				cur7seg[7:4] <= BCD7;
-				cur7seg[3:0] <= BCD6;
+				cur7Seg[11:8] <= BCD8;
+				cur7Seg[7:4] <= BCD7;
+				cur7Seg[3:0] <= BCD6;
 				show7seg = 2'b10;
 			  end
 			C:begin
-				cur7seg[11:8] <= BCD5;
-				cur7seg[7:4] <= BCD4;
-				cur7seg[3:0] <= BCD3;
+				cur7Seg[11:8] <= BCD5;
+				cur7Seg[7:4] <= BCD4;
+				cur7Seg[3:0] <= BCD3;
 				show7seg = 2'b11;
 			  end
 			D:begin
-				cur7seg[11:8] <= BCD2;
-				cur7seg[7:4] <= BCD1;
-				cur7seg[3:0] <= BCD0;
+				cur7Seg[11:8] <= BCD2;
+				cur7Seg[7:4] <= BCD1;
+				cur7Seg[3:0] <= BCD0;
 				show7seg = 2'b00;
 			  end
 			default: begin
-						cur7seg[11:8] <= 4'b1111;
-						cur7seg[7:4] <= 4'b1111;
-						cur7seg[3:0] <= 4'b1111;
+						cur7Seg[11:8] <= 4'b1111;
+						cur7Seg[7:4] <= 4'b1111;
+						cur7Seg[3:0] <= 4'b1111;
 						show7seg = 2'b01;
 					 end
 		endcase
